b64f8544295823b806c4d13fa10bdf623aa6deca
PabloSemparo <rapaliver@mail.ru>
2025-06-18 16:34:03 +0400

    Redesign work.

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,7 +6,7 @@ import path from "path";
 dotenv.config({ path: path.resolve(__dirname, "env_settings/.env.test5") });
 
 export default defineConfig({
-  testDir: "./e2e", // путь к тестам
+  testDir: "./API-Tests", // путь к тестам
   testMatch: ["**/*.spec.ts"], // можно уточнить шаблон тестов
   timeout: 1200000, // 20 минут на тест
   fullyParallel: true,
